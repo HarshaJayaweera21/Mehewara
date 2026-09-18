c26d7a255a2d088bf6f5b80f2d4d0452feac42f4
bimsara2003 <wijewanthabimsara@gmail.com>
2026-09-18 08:46:52 +0530

    fix(auth) : update the user schema to handle user profile picture

--- backend/Mehewara.API/DTOs/Auth/UserDto.cs
+++ backend/Mehewara.API/DTOs/Auth/UserDto.cs
@@ -4,9 +4,17 @@ public class UserDto
 {
     public Guid Id { get; set; }
 
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
     public string Name { get; set; } = string.Empty;
 
     public string Email { get; set; } = string.Empty;
 
     public string Role { get; set; } = string.Empty;
+
+    public string? PhoneNumber { get; set; }
+
+    public string? ProfileImageUrl { get; set; }
 }
